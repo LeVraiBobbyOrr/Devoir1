47c79630d33590f3385db7d3f534bd717d92512e
LeVraiBobbyOrr <33006532+LeVraiBobbyOrr@users.noreply.github.com>
2017-10-22 20:36:20 -0400

    Update Tictactoe.h

--- Tictactoe.h
+++ Tictactoe.h
@@ -9,7 +9,7 @@
 #define TICTACTOE_H_
 #define NBMAX 3
 
-/*Déclarations de variables globales*/
+/*DÃ©clarations de variables globales*/
 int i, j;
 int tour = 1;
 int partie;
@@ -17,8 +17,13 @@ const char JOUEUR1 = 'X';
 const char JOUEUR2 = 'O';
 int choixJoueur = 0;
 char tab[3][3] = { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
-
-/*Déclarations des méthodes*/
+char j1[25] = "personne";
+char j2[25] = "personne";
+char j3[25] = "personne";
+int s1 = 0;
+int s2 = 0;
+int s3 = 0;
+/*DÃ©clarations des mÃ©thodes*/
 void imprimer();
 void initialiserManche();
 void initialiserPartie();
@@ -26,6 +31,12 @@ void remplirTab();
 char aQuiLeTour();
 int gagnantPartie();
 int egalite();
+
+/*Declaration des structures*/
+struct{
+char nom[25];
+int score;
+};
 
 #endif /* TICTACTOE_H_ */
 
